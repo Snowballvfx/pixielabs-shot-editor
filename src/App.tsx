c288d224fe03e9e2cad10694d1e2d43db2bc97ca
itais12 <itaiitai1@gmail.com>
2025-09-16 10:50:37 +0300

    feat: remove ClipsGallery component and associated styles from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
 import { TimelineProvider } from './contexts/TimelineContext'
 import TimelineContainer from './components/TimelineContainer'
-import ClipsGallery from './components/ClipsGallery'
 
 function App() {
   return (
     <TimelineProvider>
       <div style={{ display: 'flex', height: '100vh' }}>
-        {/* Left sidebar with gallery */}
-        <div style={{ 
-          width: '200px', 
-          flexShrink: 0,
-          borderRight: '1px solid #333'
-        }}>
-          <ClipsGallery />
-        </div>
-        
         {/* Main timeline area */}
         <div style={{ flex: 1, overflow: 'hidden' }}>
           <TimelineContainer />
